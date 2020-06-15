3ea2cba4ff6c9510498aee7213340cd45e326581
Songyan Ho <hosongyan@rushowl.sg>
2020-06-15 21:24:34 +0800

    fix: fix empty

--- app/rushowl/push_notification.rb
+++ app/rushowl/push_notification.rb
@@ -7,14 +7,17 @@ class PushNotification < ApplicationMailer
     host = ENV.fetch(
         'RUSHOWL_PUSH_NOTIFICATION_HOST',
         'http://backend-rush-notification-mq.rushowl:50050/v1/notification')
+    Rails.logger.info host
+
     @conversation = conversation
     @contact = @conversation.contact
+    @identifier = @contact.identifier
 
     uri = URI.parse(host)
     push_notifications = [{user: {
         title: 'You have a new message on live chat',
         body: 'Our Customer Support Officer has sent you an message on live chat. Click here to read',
-        to: @contact.identifier,
+        to: @identifier,
         data: {
             title: 'You have a new message on live chat',
             body: 'Our Customer Support Officer has sent you an message on live chat. Click here to read',
@@ -22,8 +25,6 @@ class PushNotification < ApplicationMailer
             action_type: 'link'
         }
     }}].to_json
-
-    Rails.logger.info host
     Rails.logger.info push_notifications
 
 
