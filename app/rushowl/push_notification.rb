bbe4a7cb8f180fa8228ff1d16812888eeb915db3
Songyan Ho <hosongyan@rushowl.sg>
2020-06-15 22:03:13 +0800

    fix: fix empty

--- app/rushowl/push_notification.rb
+++ app/rushowl/push_notification.rb
@@ -4,17 +4,15 @@ require 'json'
 
 class PushNotification < ApplicationMailer
   def send_push_notification(conversation)
-    host = ENV.fetch(
+    @host = ENV.fetch(
         'RUSHOWL_PUSH_NOTIFICATION_HOST',
         'http://backend-rush-notification-mq.rushowl:50050/v1/notification')
-    Rails.logger.info host
+    Rails.logger.info @host
 
-    @conversation = conversation
-    @contact = @conversation.contact
-    @identifier = @contact.identifier
+    @identifier = conversation.contact.identifier
 
-    uri = URI.parse(host)
-    push_notifications = [{user: {
+    @uri = URI.parse(@host)
+    @push_notifications = [{user: {
         title: 'You have a new message on live chat',
         body: 'Our Customer Support Officer has sent you an message on live chat. Click here to read',
         to: @identifier,
@@ -25,14 +23,14 @@ class PushNotification < ApplicationMailer
             action_type: 'link'
         }
     }}].to_json
-    Rails.logger.info push_notifications
+    Rails.logger.info @push_notifications
 
 
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
-    request.body = push_notifications
-    response = http.request(request)
+    @http = Net::HTTP.new(@uri.host, @uri.port)
+    @request = Net::HTTP::Post.new(@uri, 'Content-Type' => 'application/json')
+    @request.body = @push_notifications
+    @response = @http.request(@request)
 
-    Rails.logger.info response
+    Rails.logger.info @response
   end
 end
